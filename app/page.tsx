52a7a1d2d1adb17c61cf5fbd02b2cd95b0b418da
dboland08 <djb221@uakron.edu>
2026-05-12 12:37:06 -0400

    Add home details and adjust global css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      Hello World
+      <h1>Daniel Boland</h1>
+      <p> Hi, I am a Computer Science graduate from the University of Akron.
+        I enjoy gridning out leetcodes, pulling all nighters trying to learn new frameworks
+        and learning as much as possible about writing clean effecient code.
+      </p>
+      <button>My GitHub</button>
     </div>
   );
 }
