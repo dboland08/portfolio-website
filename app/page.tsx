6c4147db9b16cacda84875e5b71f4d88f907b13c
dboland08 <danielboland888@gmail.com>
2026-05-14 14:37:07 -0400

    Add responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,15 @@ import "../lib/utils.css"
 export default function Home() {
   return (
     <div>
-      <h1>Daniel Boland</h1>
-      <p> Hi, I am a Computer Science graduate from the University of Akron.
-        I enjoy gridning out leetcodes, pulling all nighters trying to learn new frameworks
-        and learning as much as possible about writing clean effecient code.
-      </p>
-      <button className="btn">My GitHub</button>
+      <article className="container">
+        <h1>Daniel Boland</h1>
+        <p> Hi, I am a Computer Science graduate from the University of Akron.
+          I enjoy gridning out leetcodes, pulling all nighters trying to learn new frameworks
+          and learning as much as possible about writing clean effecient code.
+        </p>
+        <button className="btn">My GitHub</button>
+      </article>
+
     </div>
   );
 }
