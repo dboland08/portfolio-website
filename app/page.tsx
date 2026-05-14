89ac2cbd78187254ee2d6c03cd967f83a78caef3
dboland08 <danielboland888@gmail.com>
2026-05-14 15:40:17 -0400

    Add header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
 import Image from "next/image";
 import "../lib/home.css"
 import "../lib/utils.css"
+import "../components/header.css"
 
 export default function Home() {
   return (
     <div>
+      <header className="header container">
+        <nav>
+          <ul className="header__menu">
+            <li>
+              <a className="header__link" href="">Home</a>
+            </li>
+            <li>
+              <a className="header__link" href="#about">About</a>
+            </li>
+            <li>
+              <a className="header__link" href="#resume">Resume</a>
+            </li>
+            <li>
+              <a className="header__link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <article className="container">
         <h1>Daniel Boland</h1>
         <p> Hi, I am a Computer Science graduate from the University of Akron.
